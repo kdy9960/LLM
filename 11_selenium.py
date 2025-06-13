a595d3ce5f0cf748423cea81221fe9416565f857
김대영 <vjftmxmwlwhs@gmail.com>
2025-06-13 20:00:16 +0900

    크롤링 예제, 별점 추가

--- 11_selenium.py
+++ 11_selenium.py
@@ -16,10 +16,19 @@ def crawl_yanolja_reviews(name, url):
     
     review_containers = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div')
 
-    for i in range(len(review_containers)):
-        print(i)
+    review_date = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div')
 
-    
+    for i in range(len(review_containers)):
+        # print(i)
+        review_text = review_containers[i].find('P', class_='content-text').text
+        date = review_date[i].text
+        # prinat(review_text, date)
+        review_empty_stars = review_containers[i].find_all('path', {'fill-rule':''})
+        # print(review_empty_stars)
+        stars = 5 - len(review_empty_stars)
+        review_dict = {'review':review_text, 'star':stars, 'date':date}
+
+    print(review_list)    
     time.sleep(10)
 
 
